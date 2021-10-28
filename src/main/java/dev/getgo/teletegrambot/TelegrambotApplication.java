4b869a992b49133039a4c2a7887afde1d8292824
yu <szlwl01@qq.com>
2021-10-29 00:17:43 +0800

    初始化项目

---
+++ src/main/java/dev/getgo/teletegrambot/TelegrambotApplication.java
@@ -0,0 +1,16 @@
+package dev.getgo.teletegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yu
+ */
+@SpringBootApplication
+public class TelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegrambotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/getgo/teletegrambot/TeletegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.getgo.teletegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeletegrambotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

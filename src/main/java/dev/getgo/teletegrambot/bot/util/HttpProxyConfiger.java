385c0f6995ccf1e59a8f765d3f09d155ad222944
fuxy <szlwl01@qq.com>
2021-10-29 16:33:56 +0800

    处理请求走代理服务

--- src/main/java/dev/getgo/teletegrambot/bot/util/HttpProxyConfiger.java
+++ src/main/java/dev/getgo/teletegrambot/bot/util/HttpProxyConfiger.java
@@ -1,7 +1,5 @@
 package dev.getgo.teletegrambot.bot.util;
 
-import org.apache.http.HttpHost;
-import org.apache.http.client.config.RequestConfig;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 
 public class HttpProxyConfiger {
@@ -10,17 +8,6 @@ public class HttpProxyConfiger {
         defaultBotOptions.setProxyPort(10809);
         defaultBotOptions.setProxyHost("127.0.0.1");
         defaultBotOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
-
-//        RequestConfig.Builder builder = RequestConfig.copy(RequestConfig.custom().build());
-//
-//        HttpHost httpHost = new HttpHost("127.0.0.1", 10809, "http");
-//        RequestConfig requestConfig = builder
-//                .setSocketTimeout(75000)
-//                .setConnectTimeout(75000)
-//                .setConnectionRequestTimeout(75000)
-//                .setProxy(httpHost)
-//                .build();
-//        defaultBotOptions.setRequestConfig(requestConfig);
         return defaultBotOptions;
     }
 }
